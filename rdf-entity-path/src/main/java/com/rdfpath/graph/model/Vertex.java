cd455120699d759eab05a3cc0024d55e88a77384
Cristóbal T <cptorresgutierrez@gmail.com>
2023-04-12 12:58:01 -0400

    feat: Se compacta la clase

--- rdf-entity-path/src/main/java/com/rdfpath/graph/model/Vertex.java
+++ rdf-entity-path/src/main/java/com/rdfpath/graph/model/Vertex.java
@@ -1,16 +1,14 @@
 package com.rdfpath.graph.model;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Vertex {
 
-    public Integer id; // TODO hacer privado
+    public int id; // TODO hacer privado
     private List<Edge> adjacentEdges = new LinkedList<>();
-    private double degree;
-    private double degreeNormalized;
-    private double distance = Integer.MAX_VALUE;
     public ArrayList<Vertex> from; //TODO make private
     public Vertex father;
     public Vertex myFather;
@@ -18,8 +16,6 @@ public class Vertex {
     public Vertex(Integer id) {
     	this.from = new ArrayList<Vertex>();
         this.id = id;
-        this.degree = 1;
-        this.degreeNormalized = 1;
     }
 
     public void addEdge(Edge edge) {
@@ -30,33 +26,17 @@ public class Vertex {
         return id;
     }
 
-    public double getDegree() {
-        return degree;
-    }
-
-    public double getDegreeNormalized() {
-        return degreeNormalized;
-    }
-
-    public void setDegree(double degree) {
-        this.degree = degree;
-    }
-
-    public void setDegreeNormalized(double degreeNormalized) {
-        this.degreeNormalized = degreeNormalized;
-    }
-
     public List<Edge> getAdjacentEdges() {
         return adjacentEdges;
     }
     
-    public List<Vertex> getAdjacentVertex() {
+    public HashSet<Integer> getAdjacentVertex() {
     	List<Edge> actEdges = getAdjacentEdges();
-    	List <Vertex> vertexList = new LinkedList<>();
+    	HashSet<Integer> vertexSet = new HashSet<Integer>();
     	for (Edge e : actEdges) {
-    		vertexList.add(e.getOppositeVertex(this));
+    		vertexSet.add(e.getOppositeVertex(this).getId());
     	}
-    	return vertexList;
+    	return vertexSet;
     }
 
     public void setAdjacentEdges(List<Edge> adjacentEdges) {
@@ -89,47 +69,5 @@ public class Vertex {
     	}
     	return edges;
     }
-    
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Vertex other = (Vertex) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
-    }
-    
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-    
-    @Override
-    public String toString() {
-        return id.toString();
-    }
-
-	public void setDistance(double d) {
-        this.distance = d;
-    }
-
-	public double getDistance() {
-		return this.distance;
-	}
-	
-	public double getBaselineWeight () {
-		return 1;
-	}
 
 }
