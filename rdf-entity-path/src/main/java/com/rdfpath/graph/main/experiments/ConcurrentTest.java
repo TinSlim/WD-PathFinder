b5a3512db0e09af04850b1ec3fc82a3d1c46051a
Cristóbal <cptorresgutierrez@gmail.com>
2023-04-22 01:35:11 -0400

    feat: Test de obtención de caminos

--- rdf-entity-path/src/main/java/com/rdfpath/graph/main/experiments/ConcurrentTest.java
+++ rdf-entity-path/src/main/java/com/rdfpath/graph/main/experiments/ConcurrentTest.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package com.rdfpath.graph.main;
+package com.rdfpath.graph.main.experiments;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,7 +9,6 @@ import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
 import java.lang.management.MemoryUsage;
 import java.lang.ref.WeakReference;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -17,11 +16,6 @@ import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-import com.rdfpath.graph.model.Graph;
-import com.rdfpath.graph.model.GraphComp;
-import com.rdfpath.graph.model.GraphCompDense;
-import com.rdfpath.graph.model.GraphFullNative;
-import com.rdfpath.graph.model.GraphNative;
 import com.rdfpath.graph.model.GraphNativeFullDense;
 import com.rdfpath.graph.model.GraphWrapper3;
 import com.rdfpath.graph.model.IGraph;
@@ -39,22 +33,23 @@ public class ConcurrentTest {
 	public static void main (String[] args) throws IOException {
 
 		String path = "subsets/";
-		String[] files = {"subset100000", "subset1000000", "subset10000000", "subset100000000","latest-truthy_small"};//{"subset10000000"};//{"subset100000", "subset1000000", "subset10000000"};
+		String[] files = {"subset100000", "subset1000000", "subset10000000", "subset100000000","latest-truthy_small"};
 		String end = ".nt.gz";
-		String endComp = "_compressed.gz";
+		//String endComp = "_compressed.gz";
 		String endNative = "_native.gz";
 		
 		int [] nodesSize = {92654, 829794, 8159611, 87110322, 99609308};
-		int [] maxNodeId = {100000, 1000000, 10000000, 99999996, 117288116};
+		//int [] maxNodeId = {100000, 1000000, 10000000, 99999996, 117288116};
 		int [] edgesSize = {600493, 5977585, 42682387, 652319619, 715906922};
 		
 		int index = 1;
 		
 		IGraph graph;
+		System.out.println("Loading graph");
 		graph = new GraphNativeFullDense(path + files[index] + end, path + files[index] + endNative, true, true, edgesSize[index], nodesSize[index]);
 		System.out.println("GraphLoaded");
+	
 		
-		/*
 		BufferedReader fileBuff = Utils.readFile(path+files[index]+"_random_pairs.csv",false);
 		String line = "";
         String[] tempArr;
@@ -67,29 +62,28 @@ public class ConcurrentTest {
         	ids[j][0] = Integer.parseInt(tempArr1[0]);
         	ids[j][1] = Integer.parseInt(tempArr1[1]);
         }
-		*/
+
 		
 		int i = 0;
-		ExecutorService executor;
+		System.out.println("Estructura;ID;Aristas;Tiempo;Memoria");
+		MemoryMXBean mbean = ManagementFactory.getMemoryMXBean();
 		
-		while (i < 5) {
-			executor = Executors.newSingleThreadExecutor();
-			int[] a = {261, 298};
-			GraphWrapper3 nGW = new GraphWrapper3(graph);
-			nGW.search(a, 3);
+		while (i < ids.length) {
 			
-			Future future = executor.submit(new LongRunningTask(nGW));
-			//try {
-			//    future.get(12, TimeUnit.SECONDS);
-			//} catch (TimeoutException e) {
-			//    future.cancel(true);
-			//} catch (Exception e) {
-			//    // handle other exceptions
-			//} finally {
-			//   executor.shutdownNow();
-			//}
-			//lrt.stop();
-			System.out.println("Terminó el: " + i);
+			
+			System.out.print(graph.getStructName()+";"+i+";");
+			
+			long memoryBefore = mbean.getHeapMemoryUsage().getUsed();
+			int[] a = {ids[i][0], ids[i][1]};
+			GraphWrapper3 nGW = new GraphWrapper3(graph);
+			WeakReference a1 = new WeakReference(nGW);
+			nGW.search(a, 3);
+			System.out.println((mbean.getHeapMemoryUsage().getUsed() - memoryBefore));
+			nGW = null;
+
+			while (a1.get() != null) {
+				System.gc();
+			}
 			i++;
 		}
 		
--- rdf-entity-path/src/main/java/com/rdfpath/graph/main/experiments/TimeTest.java
+++ rdf-entity-path/src/main/java/com/rdfpath/graph/main/experiments/TimeTest.java
@@ -1,4 +1,4 @@
-package com.rdfpath.graph.main;
+package com.rdfpath.graph.main.experiments;
 
 import java.io.FileReader;
 import java.io.FileWriter;
