46d9e1d79e39abcded4a3782cb622ad0124d9dab
Cristóbal T <cptorresgutierrez@gmail.com>
2023-04-24 15:29:49 -0400

    fix: Corrige clases de tests

---
+++ rdf-entity-path/src/main/java/com/rdfpath/graph/main/experiments/PathFindTest.java
@@ -0,0 +1,120 @@
+/**
+ * 
+ */
+package com.rdfpath.graph.main.experiments;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.ref.WeakReference;
+import com.rdfpath.graph.model.Graph;
+import com.rdfpath.graph.model.GraphComp;
+import com.rdfpath.graph.model.GraphCompDense;
+import com.rdfpath.graph.model.GraphFullNative;
+import com.rdfpath.graph.model.GraphNative;
+import com.rdfpath.graph.model.GraphNativeFullDense;
+import com.rdfpath.graph.model.GraphWrapper3;
+import com.rdfpath.graph.model.IGraph;
+import com.rdfpath.graph.utils.Utils;
+
+/**
+ *
+ * @author Cristóbal Torres G.
+ * @github Tinslim
+ *
+ */
+public class PathFindTest {
+
+	  
+	public static void main (String[] args) throws IOException {
+
+		if (System.getProperty("subset") == null || System.getProperty("graph") == null) {
+			System.out.println("-Dsubset -Dgraph");
+			return;
+		}		
+
+		String path = "subsets/";
+		String[] files = {"subset100000", "subset1000000", "subset10000000", "subset100000000","latest-truthy_small"};
+		String end = ".nt.gz";
+		String endComp = "_compressed.gz";
+		String endNative = "_native.gz";
+		
+		int [] nodesSize = {92654, 829794, 8159611, 87110322, 99609308};
+		int [] maxNodeId = {100000, 1000000, 10000000, 99999996, 117288116};
+		int [] edgesSize = {600493, 5977585, 42682387, 652319619, 715906922};
+
+		int index = 0;
+		String actSubset = System.getProperty("subset");
+		String graphName = System.getProperty("graph");
+		while (index < files.length) {
+			if (files[index].equals(actSubset)) {
+				break;
+			}
+			index += 1;
+		}	
+		
+		IGraph graph;
+		
+		if (graphName.equals("graphGt")) {
+			graph = new Graph(path + files[index] + end, true);
+		}
+		else if (graphName.equals("graphNative")) {
+			graph = new GraphNative(path + files[index] + end, true, edgesSize[index]);
+		}
+		else if (graphName.equals("graphCompDense")) {
+			graph = new GraphCompDense(path + files[index] + endComp, true, nodesSize[index]);
+		}
+		else if (graphName.equals("graphComp")) {
+			graph = new GraphComp(path + files[index] + endComp, true, maxNodeId[index]);
+		}
+		else if (graphName.equals("graphNativeFull")) {
+			graph = new GraphFullNative(path + files[index] + end, path + files[index] + endNative, true, true, edgesSize[index], maxNodeId[index]);
+		}
+		else if (graphName.equals("graphNativeFullDense")) {
+			graph = new GraphNativeFullDense(path + files[index] + end, path + files[index] + endNative, true, true, edgesSize[index], nodesSize[index]);
+		}
+		else {
+			System.out.println("Fail");
+			return;
+		}	
+		
+		BufferedReader fileBuff = Utils.readFile(path+files[index]+"_random_pairs.csv",false);
+		String line = "";
+        String[] tempArr;
+        line = fileBuff.readLine();
+        tempArr = line.split(";");
+        int [][] ids = new int[100][2];
+        String[] tempArr1;
+        for (int j = 0; j < tempArr.length; j++) {
+        	tempArr1 = tempArr[j].split(",");
+        	ids[j][0] = Integer.parseInt(tempArr1[0]);
+        	ids[j][1] = Integer.parseInt(tempArr1[1]);
+        }
+
+		
+		int i = 0;
+		System.out.println("Estructura;ID;Aristas;Tiempo;Memoria");
+		MemoryMXBean mbean = ManagementFactory.getMemoryMXBean();
+		
+		while (i < ids.length) {
+			
+			
+			System.out.print(graph.getStructName()+";"+i+";");
+			
+			long memoryBefore = mbean.getHeapMemoryUsage().getUsed();
+			int[] a = {ids[i][0], ids[i][1]};
+			GraphWrapper3 nGW = new GraphWrapper3(graph);
+			WeakReference a1 = new WeakReference(nGW);
+			nGW.search(a, 3);
+			System.out.println((mbean.getHeapMemoryUsage().getUsed() - memoryBefore));
+			nGW = null;
+
+			while (a1.get() != null) {
+				System.gc();
+			}
+			i++;
+		}
+		
+	}
+}
--- rdf-entity-path/src/main/java/com/rdfpath/graph/model/GraphWrapper3.java
+++ rdf-entity-path/src/main/java/com/rdfpath/graph/model/GraphWrapper3.java
@@ -42,90 +42,115 @@ public class GraphWrapper3 {
 		this.session = session;
 	}
 
+	public void checkTime (String message) throws Exception {
+		if ( (System.currentTimeMillis() - startTime) > seconds * 1000 ) {
+			if (System.getProperty("debug") != null) {
+				System.out.println(message);
+			}
+			throw new Exception("Done Time");
+		}
+	}
+	
 	//@Override
     //public void run() {
 	public void search (int [] nodesNumbers, int size) {
 		startTime = System.currentTimeMillis();
-		LinkedList<VertexWrapper2> toSearch = new LinkedList<VertexWrapper2>();
-		HashSet<Integer> nodesNumbersSet = new HashSet<Integer>();
-		
-		// Los añade a lista para buscar
-		for (int idSearch : nodesNumbers) {
-			VertexWrapper2 actVW = new VertexWrapper2(idSearch);
-			nodes.put(idSearch, actVW);
-			nodesNumbersSet.add(idSearch);
-			toSearch.push(actVW);
-		}
-
-		while ( toSearch.size() > 0 && ((System.currentTimeMillis() - startTime) < seconds * 1000) ) {
-			VertexWrapper2 actualVW = toSearch.pop();
+		try {
+			LinkedList<VertexWrapper2> toSearch = new LinkedList<VertexWrapper2>();
+			HashSet<Integer> nodesNumbersSet = new HashSet<Integer>();
 			
-			if (actualVW.sameColorDistance > (size/2) + size%2) {
-				continue;
+			// Los añade a lista para buscar
+			for (int idSearch : nodesNumbers) {
+				VertexWrapper2 actVW = new VertexWrapper2(idSearch);
+				nodes.put(idSearch, actVW);
+				nodesNumbersSet.add(idSearch);
+				toSearch.push(actVW);
 			}
-			
-			// Revisa VÉRTICES adyacentes
-			for (Integer adjVertex : graph.getAdjacentVertex(actualVW.idVertex)) {
+	
+			while ( toSearch.size() > 0) {
+				checkTime("Before pop");													// Revisa tiempo
 				
-				// NO ha sido visitado:
-				if (nodes.get(adjVertex) == null) {
-					VertexWrapper2 newVW = new VertexWrapper2 (actualVW, adjVertex);
-					nodes.put(adjVertex, newVW);
-					toSearch.add(newVW);
+				VertexWrapper2 actualVW = toSearch.pop();
+				
+				if (actualVW.sameColorDistance > (size/2) + size%2) {
+					continue;
 				}
 				
-				// SI ha sido visitado
-				else {
-					VertexWrapper2 adjVW = nodes.get(adjVertex);
+				// Revisa VÉRTICES adyacentes
+				for (Integer adjVertex : graph.getAdjacentVertex(actualVW.idVertex)) {
+					checkTime("Checking adj vertexes");												// Revisa tiempo
 					
-					// Si es el que lo agregó a la lista
-					if (actualVW.fromFather(adjVW)) {
-						actualVW.removeFather();
+					// Así no cicla en el mismo nodo
+					if (actualVW.idVertex == adjVertex) {
 						continue;
 					}
 					
-					// Mismo color
-					if (actualVW.colorNode == adjVW.colorNode) {
-
-						// Revisar si se agregó, ESTÁ EN PATH
-						if (adjVW.otherColorDistance > -1 && adjVW.otherColorDistance + actualVW.sameColorDistance <= size) {
-							LinkedList<Integer> unionNodes = new LinkedList<Integer>();
-							unionNodes.push(adjVW.idVertex);
-							unionNodes.push(actualVW.idVertex);
-							makeEdges(unionNodes);
-							backTracking(actualVW, size, nodesNumbersSet);
+					// NO ha sido visitado:
+					if (nodes.get(adjVertex) == null) {
+						VertexWrapper2 newVW = new VertexWrapper2 (actualVW, adjVertex);
+						nodes.put(adjVertex, newVW);
+						toSearch.add(newVW);
+					}
+					
+					// SI ha sido visitado
+					else {
+						VertexWrapper2 adjVW = nodes.get(adjVertex);
+						
+						// Si es el que lo agregó a la lista
+						if (actualVW.fromFather(adjVW)) {
+							actualVW.removeFather();
+							continue;
 						}
 						
-						else if (adjVW.addFrom(actualVW)) {
-							toSearch.add(adjVW);
+						// Mismo color
+						if (actualVW.colorNode == adjVW.colorNode) {
+	
+							// Revisar si se agregó, ESTÁ EN PATH
+							if (adjVW.otherColorDistance > -1 && adjVW.otherColorDistance + actualVW.sameColorDistance <= size) {
+								LinkedList<Integer> unionNodes = new LinkedList<Integer>();
+								unionNodes.push(adjVW.idVertex);
+								unionNodes.push(actualVW.idVertex);
+								makeEdges(unionNodes);
+								backTracking(actualVW, size, nodesNumbersSet);
+							}
+							
+							else if (adjVW.addFrom(actualVW)) {
+								toSearch.add(adjVW);
+							}
 						}
-					}
-					
-					else {
-						if (adjVW.sameColorDistance + actualVW.sameColorDistance + 1 <= size) { //
-							adjVW.otherColorDistance = actualVW.sameColorDistance + 1;
-							actualVW.otherColorDistance = adjVW.sameColorDistance + 1;
-							backTracking(adjVW, size, nodesNumbersSet);
-							LinkedList<Integer> unionNodes = new LinkedList<Integer>();
-							unionNodes.push(adjVW.idVertex);
-							unionNodes.push(actualVW.idVertex);
-							makeEdges(unionNodes);
-							backTracking(actualVW, size, nodesNumbersSet);
+						
+						else {
+							if (adjVW.sameColorDistance + actualVW.sameColorDistance + 1 <= size) { //
+								adjVW.otherColorDistance = actualVW.sameColorDistance + 1;
+								actualVW.otherColorDistance = adjVW.sameColorDistance + 1;
+								backTracking(adjVW, size, nodesNumbersSet);
+								LinkedList<Integer> unionNodes = new LinkedList<Integer>();
+								unionNodes.push(adjVW.idVertex);
+								unionNodes.push(actualVW.idVertex);
+								makeEdges(unionNodes);
+								backTracking(actualVW, size, nodesNumbersSet);
+							}
+						
 						}
-					
+						
 					}
-					
 				}
 			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			//e.printStackTrace();
 		}
-		System.out.print(totalEdges+";"+(System.currentTimeMillis() - startTime)+";");
-
+		
+		finally {
+			System.out.print(totalEdges+";"+(System.currentTimeMillis() - startTime)+";");
+		}
 	}
 	
-	public void backTracking(VertexWrapper2 vw, int maxSize, HashSet<Integer> nodesNumbers) {
+	public void backTracking(VertexWrapper2 vw, int maxSize, HashSet<Integer> nodesNumbers) throws Exception {
 		LinkedList<VertexBackTracking> stack = new LinkedList<VertexBackTracking>();
 		stack.push(new VertexBackTracking(vw));
-		while (stack.size() > 0  && (System.currentTimeMillis() - startTime) < seconds * 1000) {
+		while (stack.size() > 0) {
+			checkTime("Inside backTracking");														// Revisa tiempo
 			VertexBackTracking actualBT = stack.pop();
 			if (actualBT.colorDistance + actualBT.grade > maxSize) {
 				continue;
@@ -136,6 +161,7 @@ public class GraphWrapper3 {
 			}
 			
 			for (VertexWrapper2 vwFrom: actualBT.actVW.from) {
+				checkTime("For vertex in BT");
 				if (! actualBT.nodes.contains(vwFrom.idVertex)) {
 					VertexBackTracking vbtNew = new VertexBackTracking(actualBT, vwFrom);
 					stack.push(vbtNew);
@@ -144,19 +170,20 @@ public class GraphWrapper3 {
 		}
 	}
 
-	public void makeEdges (LinkedList<Integer> nodesList) {
+	public void makeEdges (LinkedList<Integer> nodesList) throws Exception {
 		if (nodesList.size() < 2) {
 			return;
 		}
 		int i = 0;
-		while ( (i < nodesList.size() - 1) && ((System.currentTimeMillis() - startTime) < seconds * 1000)) {
+		while ( i < nodesList.size() - 1) {
+			checkTime("Inside makeEdges");														// Revisa tiempo
 			sendEdges(nodesList.get(i), nodesList.get(i + 1));
 			i++;
 		}
 		return;
 	}
 	
-	public void sendEdges (int v1, int v2) {
+	public void sendEdges (int v1, int v2) throws Exception {
 		if ((nodes.get(v1).hasEdgeWith(v2) && nodes.get(v2).hasEdgeWith(v1)) || ((System.currentTimeMillis() - startTime) >= seconds * 1000)) {
 			return;
 		}
@@ -176,12 +203,10 @@ public class GraphWrapper3 {
 			addedNodes.add(v2);
 		}
 		
-		if ((System.currentTimeMillis() - startTime) >= seconds * 1000) {
-			return;
-		}
 		
 		for (Object edge : edges) {
-			totalEdges+=1;		
+			checkTime("For edge in sendEdges");														// Revisa tiempo
+			totalEdges+=1;
 		}
 		
 	}
