18e41cd14e7b128b39629aebf73eb31699265413
Cristóbal <cptorresgutierrez@gmail.com>
2023-06-13 00:48:30 -0400

    fix: Corrige traducción

--- frontend/src/components/i18n.js
+++ frontend/src/components/i18n.js
@@ -10,7 +10,7 @@ const resources = {
       "English": "English",
       "Spanish": "Spanish",
 
-      "Description": "Woolnet, es el graficador de caminos entre entidades de Wikidata",
+      "Description": "",
       "Time": "Time",
       "Menu": "Menu",
       "Stop": "STOP",
@@ -39,7 +39,7 @@ const resources = {
       "English": "Inglés",
       "Spanish": "Español",
 
-      "Description": "",
+      "Description": "Woolnet, es el graficador de caminos entre entidades de Wikidata",
       "Time": "Tiempo",
       "Menu": "Menú",
       "Stop": "DETENER",
