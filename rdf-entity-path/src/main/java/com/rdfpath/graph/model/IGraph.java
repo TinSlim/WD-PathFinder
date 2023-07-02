994f6a1e7d61179d26f40ad5e0c1208f8f2a6560
Cristóbal <cptorresgutierrez@gmail.com>
2023-07-01 23:08:55 -0400

    clean: Añade firmas

--- rdf-entity-path/src/main/java/com/rdfpath/graph/model/IGraph.java
+++ rdf-entity-path/src/main/java/com/rdfpath/graph/model/IGraph.java
@@ -57,23 +57,16 @@ public interface IGraph {
 	public int getDestinationEdge(Object e);
 
 	/**
-	 * @throws ParseException 
-	 * 
+	 * Imprime en un escritor los tiempos de búsqueda de vecinos de los nodos.
+	 * @param ids						nodos de los que se van a buscar sus vecinos
+	 * @param pw						escritor
+	 * @param dataSet					nombre del set de datos usado
 	 */
-	public void diffGroups() throws ParseException;
-	
-	/**
-	 * @throws ParseException 
-	 * 
-	 */
-	public void sameGroups() throws ParseException;
-
 	public void writeSearchAdj(int[] ids, PrintWriter pw, String dataSet);
 
 	/**
-	 * Obtiene el nombre de la estructura.
-	 * 
-	 * @return				Nombre de la estructura
+	 * Devuelve el nombre de la estructura
+	 * @return							Nombre de la estructura
 	 */
 	public String getStructName();
 
@@ -89,21 +82,63 @@ public interface IGraph {
 	int getPredicateEdge(Object e);
 
 
-
+	/**
+	 * Obtiene los nodos adyacentes de un nodo. Si la cantidad de segundos de la búsqueda se supera lanza una interrupción.
+	 * @param id							id del nodo que se obtendrán sus vecinos
+	 * @param seconds						segundos máximos de búsqueda
+	 * @param startTime						tiempo de inicio
+	 * @return								HashSet con los ids de nodos vecinos
+	 * @throws InterruptedException			Interrupción cuando se supera el tiempo máximo
+	 */
 	HashSet<Integer> getAdjacentVertexTimeout(int id, int seconds, long startTime) throws InterruptedException;
 
+	/**
+	 * Obtiene los nodos adyacentes de un nodo. Si la cantidad de segundos de la búsqueda se supera lanza una interrupción.
+	 * Si el nodo actual tiene más vecinos que límite máximo y no es inicial, no se retorna nodos vecinos.
+	 * @param id							id del nodo que se obtendrán sus vecinos
+	 * @param seconds						segundos máximos de búsqueda
+	 * @param startTime						tiempo de inicio
+	 * @param maxEdgeSize					límite de vecinos para el nodo
+	 * @param isInitial						si el nodo es inicial o no
+	 * @return								HashSet con los ids de nodos vecinos
+	 * @throws InterruptedException			Interrupción cuando se supera el tiempo máximo
+	 */
 	HashSet<Integer> getAdjacentVertexTimeoutLimited(int id, int seconds, long startTime, int maxEdgeSize, boolean isInitial) throws InterruptedException;
 	
 	/**
-	 * @param ids
-	 * @return
+	 * Devuelve un HashSet con llave el id del nodo y valor sus nodos vecinos.
+	 * @param ids							id de los nodos que se buscarán vecinos
+	 * @return								HashSet con llave el id del nodo y valor sus nodos vecinos
 	 */
 	public ArrayList checkAdj(int[] ids);
 
+	
+	/**
+	 * Obtiene los nodos adyacentes de un nodo. Si la sessión no sigue abierta lanza una interrupción.
+	 * @param id							id del nodo que se obtendrán sus vecinos
+	 * @param session						conexión actual
+	 * @return								HashSet con los ids de nodos vecinos
+	 * @throws IOException					Interrupción cuando no sigue la conexión
+	 */
 	HashSet<Integer> getAdjacentVertexSession(int id, WebSocketSession session) throws IOException;
 	
+	/**
+	 * Obtiene los nodos adyacentes de un nodo. Si la sessión no sigue abierta lanza una interrupción. 
+	 * Si el nodo actual tiene más vecinos que límite máximo y no es inicial, no se retorna nodos vecinos.
+	 * @param id							id del nodo que se obtendrán sus vecinos
+	 * @param session						conexión actual
+	 * @param maxEdgeSize					límite de vecinos para el nodo
+	 * @param isInitial						si el nodo es inicial o no
+	 * @return								HashSet con los ids de nodos vecinos
+	 * @throws IOException					Interrupción cuando no sigue la conexión
+	 */
 	HashSet<Integer> getAdjacentVertexSessionLimited(int id, WebSocketSession session, int maxEdgeSize, boolean isInitial) throws IOException;
 	
+	/**
+	 * Obtiene el grado del nodo, es decir cantidad de nodos vecinos.
+	 * @param idVertex						nodo que se le obtendrá el grado
+	 * @return								grado del nodo
+	 */
 	public int getGrade (int idVertex);
 
 }
